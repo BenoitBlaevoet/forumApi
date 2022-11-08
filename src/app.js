c879efac0a81c50ae199a5dba373de687ebbcd79
Blaevoet Benoit <blaevoet.benoit@outlook.com>
2022-11-08 16:07:11 +0100

    added handle of host env

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const fastify = require('fastify')({ logger: true })
 const middie = require('@fastify/middie')
 const cors = require('@fastify/cors')
 const moment = require('moment')
+require('dotenv').config()
 const diffForHumans = require('./scripts/diffForHumans')
 
 const fs = require('fs')
@@ -34,9 +35,11 @@ errorDir.forEach(route => {
 })
 
 // start server
+const serverPort = process.env.PORT || 3000
+const serverHost = process.env.HOST || '0.0.0.0'
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 3000 })
+    await fastify.listen({ port: serverPort, host: serverHost })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
